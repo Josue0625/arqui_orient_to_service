837cf7d654efe0e0abf362f6edcb73cfb81a3a47
Josue0625 <jcampos@ufpso.edu.co>
2023-08-29 19:32:28 -0500

    se hizo la logica para generar token, en el auth

---
+++ back/controllers/auth.controllers.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken"
+import { exports } from "../config/default.js"
+export const login = async (req, res) => {
+    const token = jwt.sign({
+        exp: Math.floor(Date.now() / 1000) + (60 * 60),
+        data:{
+            username: 'jos',
+            role: 'admin',
+        }
+    }, exports.key)
+    res.status(200).json({
+        success: true,
+        token: token,
+    })
+}
--- back/validators/producto.validators.js
+++ back/validators/producto.validators.js
@@ -4,5 +4,6 @@ export const productoValidator = checkSchema({
     nombre: {
         notEmpty: true,
         errorMessage: "El nombre es requerido",
+        matches: {options: /[0-9]/}
     }
 }, ["query"]);
