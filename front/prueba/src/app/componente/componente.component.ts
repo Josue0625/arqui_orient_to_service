7349be8c7f9fa5525574a036b93ecd580434df0f
Josue0625 <jcampos@ufpso.edu.co>
2023-09-05 19:47:19 -0500

    Tercera clase en la que se hicieron los componentes del front

---
+++ front/prueba/src/app/componente/componente.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente',
+  template: `<div>hola componente</div>`,
+})
+export class componenteComponent {
+  title = 'componente';
+}
