3d90feebf0cc5f077f6124c9c2583aaa2576101a
Josue0625 <jcampos@ufpso.edu.co>
2023-09-19 19:35:30 -0500

    se hicieron formularios normales utilizando ngForm y tambien se hizo otrro formulario usando el RectiveFormulario

--- front/prueba/src/app/app.module.ts
+++ front/prueba/src/app/app.module.ts
@@ -1,17 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {componenteComponent} from './componente/componente.component';
-import { PrimeroComponent } from './primero/primero.component'
+import { PrimeroComponent } from './primero/primero.component';
+import { SegundoComponent } from './segundo/segundo.component';
+import { TerceroComponent } from './tercero/tercero.component'
 @NgModule({
   declarations: [
     AppComponent,
     componenteComponent,
-    PrimeroComponent
+    PrimeroComponent,
+    SegundoComponent,
+    TerceroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front/prueba/src/app/segundo/segundo.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-segundo',
+  templateUrl: './segundo.component.html',
+  styleUrls: ['./segundo.component.scss']
+})
+export class SegundoComponent {
+
+  formulario = {
+    nombre: '',
+    valor: 0
+  }
+
+  enviar(){
+    console.log(this.formulario)
+  }
+
+}
